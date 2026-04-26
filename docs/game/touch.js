3e379acc6d956167959732cc94a214d2489ccb7c
Chuong Mai <chuongmai@yahoo.com>
2026-04-26 16:07:40 -0400

    Increase tap tolerance for finger tip

--- docs/game/touch.js
+++ docs/game/touch.js
@@ -6,7 +6,7 @@ let touchStartTime = 0;
 let lastTapTime = 0;
 
 const SWIPE_MIN_DISTANCE = 30;
-const TAP_MAX_DISTANCE = 10;
+const TAP_MAX_DISTANCE = 20;
 const TAP_MAX_TIME = 250;
 
 let touchDirection = null;
